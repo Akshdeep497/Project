2ed1d99fc0ef01831322daf7cae8919b6f576cda
Akshdeep497 <136171308+Akshdeep497@users.noreply.github.com>
2024-10-20 02:20:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,14 +4,7 @@ import streamlit as st
 import os
 from PIL import Image
 import google.generativeai as genai
-hide_st_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            header {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_st_style, unsafe_allow_html=True)
+
 # Load environment variables
 load_dotenv()
 
