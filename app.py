fba2fb781e2e6ffcc27cabf313966320b7e77616
Akshdeep497 <136171308+Akshdeep497@users.noreply.github.com>
2024-10-21 13:16:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -73,11 +73,11 @@ if response:
     st.subheader("Response")
     st.write(response)
 
-hide_st_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            header {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_st_style, unsafe_allow_html=True)
+# hide_st_style = """
+#             <style>
+#             #MainMenu {visibility: hidden;}
+#             footer {visibility: hidden;}
+#             header {visibility: hidden;}
+#             </style>
+#             """
+# st.markdown(hide_st_style, unsafe_allow_html=True)
